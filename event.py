7b0049cba7ec8805c25ed700af8de95908a0019a
taylornich <taylormanhard15@gmail.com>
2024-07-23 23:01:43 -0700

    Question 2

---
+++ event.py
@@ -0,0 +1,11 @@
+class Event:
+    def __init__(self, name, date):
+        self.name = name
+        self.date = date
+        self.participant_count = 0
+
+    def add_participant(self):
+        self.participant_count +=1
+
+    def get_participant_count(self):
+        return self.participant_count
